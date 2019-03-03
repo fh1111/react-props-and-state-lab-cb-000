3ecdf974344420e5d3c0d4cf80078aacbc46cf75
Fer Hernandez <fh.1111@gmail.com>
2019-03-03 15:13:32 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -38,25 +38,27 @@ class App extends React.Component {
   this.setState({ pets });
 };
 
-  render() {
-    return (
+render() {
+  return (
+    <div className="ui container">
+      <header>
+        <h1 className="ui dividing header">React Animal Shelter</h1>
+      </header>
       <div className="ui container">
-        <header>
-          <h1 className="ui dividing header">React Animal Shelter</h1>
-        </header>
-        <div className="ui container">
-          <div className="ui grid">
-            <div className="four wide column">
-              <Filters />
-            </div>
-            <div className="twelve wide column">
-              <PetBrowser />
-            </div>
+        <div className="ui grid">
+          <div className="four wide column">
+            <Filters onChangeType={this.onChangeType} onFindPetsClick={this.fetchPets} />
+          </div>
+          <div className="twelve wide column">
+            <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  );
+}
+
+
 }
 
 export default App
