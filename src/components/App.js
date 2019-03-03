6fc27dd73ea6b05d7dd384bbd311c7285621372a
Fer Hernandez <fh.1111@gmail.com>
2019-03-03 15:13:52 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -32,31 +32,31 @@ class App extends React.Component {
   };
 
   onAdoptPet = petId => {
-  const pets = this.state.pets.map(p => {
-    return p.id === petId ? { ...p, isAdopted: true } : p;
-  });
-  this.setState({ pets });
-};
+    const pets = this.state.pets.map(p => {
+      return p.id === petId ? { ...p, isAdopted: true } : p;
+    });
+    this.setState({ pets });
+  };
 
-render() {
-  return (
-    <div className="ui container">
-      <header>
-        <h1 className="ui dividing header">React Animal Shelter</h1>
-      </header>
+  render() {
+    return (
       <div className="ui container">
-        <div className="ui grid">
-          <div className="four wide column">
-            <Filters onChangeType={this.onChangeType} onFindPetsClick={this.fetchPets} />
-          </div>
-          <div className="twelve wide column">
-            <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
+        <header>
+          <h1 className="ui dividing header">React Animal Shelter</h1>
+        </header>
+        <div className="ui container">
+          <div className="ui grid">
+            <div className="four wide column">
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.fetchPets} />
+            </div>
+            <div className="twelve wide column">
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
 
 }
